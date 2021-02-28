8370a607993df9423e6ee8dd670c1c5e4794b903
Éverton <evertonend12@gmail.com>
2021-02-28 00:09:26 -0300

    criando e estilizando componente ExperienceBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import React from "react";
+import { ExperienceBar } from "./components/ExperienceBar";
 import "./styles/global.css";
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <h1>No CAPRICHOOO!!!</h1>
+      <ExperienceBar></ExperienceBar>
     </div>
   );
 }
