05d24c324d37ecf4fdaf1f88d680091c533140cd
Éverton <evertonend12@gmail.com>
2021-02-27 23:09:24 -0300

    descobrindo componentes, propriedades e estado

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Button } from "./components/Button";
+
 function App() {
-  return <h1>No CAPRICHOOO!!!</h1>;
+  return (
+    <div>
+      <h1>No CAPRICHOOO!!!</h1>
+      <Button color="green">Green</Button>
+      <Button color="pink">Pink</Button>
+    </div>
+  );
 }
 
 export default App;
