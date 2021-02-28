248e2831f4e20b8dba07facebab66ed30a43c2b8
Éverton <evertonend12@gmail.com>
2021-02-27 23:48:47 -0300

    criando globalCss e definindo algumas variaveis para cores, font e responsividade

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { Button } from "./components/Button";
+import "./styles/global.css";
 
 function App() {
   return (
     <div>
       <h1>No CAPRICHOOO!!!</h1>
-      <Button color="green">Green</Button>
-      <Button color="pink">Pink</Button>
     </div>
   );
 }
